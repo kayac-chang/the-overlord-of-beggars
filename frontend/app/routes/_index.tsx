981df1db6f140cf67c5338b4505c65fff1d2151a
kayac-chang <egg734631@gmail.com>
2024-10-15 22:32:52 +0800

    feat: get user geolocation and setup request flow

--- frontend/app/routes/_index.tsx
+++ frontend/app/routes/_index.tsx
@@ -1,5 +1,9 @@
-import type { MetaFunction } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import type { LoaderFunctionArgs, MetaFunction } from "@remix-run/node";
+import {
+  ClientLoaderFunctionArgs,
+  redirect,
+  useLoaderData,
+} from "@remix-run/react";
 import { StoreWithNearExpiredFoodSchema } from "~/models/store_with_near_expired_foods";
 import getNearExpiredFoodsByStoreId from "~/services/get_near_expired_foods_by_store_id";
 import searchStoresByGeoLocation from "~/services/search_stores_by_geolocation";
@@ -11,13 +15,22 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-export async function loader() {
+export async function loader({ request }: LoaderFunctionArgs) {
+  const url = new URL(request.url);
+
+  const location = url.searchParams.get("location");
+
+  // if the location is not provided, we'll just display the default page
+  if (!location) {
+    return null;
+  }
+
+  // @todo: regex to validate geolocation
+  const [latitude, longitude] = location.split(",").map(Number);
+
   // 透過經緯度搜尋附近的店家
   const stores = await searchStoresByGeoLocation({
-    location: {
-      latitude: 24.986227,
-      longitude: 121.4534552,
-    },
+    location: { latitude, longitude },
   });
 
   // 取得每家店的即將過期的食物
@@ -31,9 +44,43 @@ export async function loader() {
   );
 }
 
+export function clientLoader({
+  request,
+  serverLoader,
+}: ClientLoaderFunctionArgs) {
+  const url = new URL(request.url);
+
+  // if the location is already provided in the URL, we'll just load the data from the server
+  if (url.searchParams.has("location")) {
+    return serverLoader<typeof loader>();
+  }
+
+  return (
+    new Promise<GeolocationPosition>((resolve, reject) =>
+      navigator.geolocation.getCurrentPosition(resolve, reject)
+    )
+      // if user grants location permission, we'll redirect to the page with the location
+      .then((position) =>
+        redirect(
+          new URLSearchParams({
+            location: `${position.coords.latitude},${position.coords.longitude}`,
+          }).toString()
+        )
+      )
+      // otherwise. user denies location permission, we'll just show the default page
+      .catch(() => null)
+  );
+}
+clientLoader.hydrate = true;
+
 export default function Index() {
   const data = useLoaderData<typeof loader>();
-  console.log(data);
+
+  // when the user denies location permission
+  // the default page will be rendered
+  if (!data) return null;
+
+  // display the nearby stores and their near expired foods
   return (
     <>
       <ul>
