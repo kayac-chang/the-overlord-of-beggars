8f0f3f876330d04c9c7bc8286fdf4734c57882c4
TANG CHENG LEE <TANGCHENGLEE@virgil246.com>
2024-10-26 20:20:28 +0800

    move validator into family_mart/model.py

--- backend/app/data_sources/custom_types.py
+++
@@ -1,9 +0,0 @@
-import unicodedata
-from pydantic import BeforeValidator
-from typing_extensions import Annotated
-
-
-def fWidth2hWidth(source:str):
-    return unicodedata.normalize('NFKC', source)
-
-CustomAddress=Annotated[str,BeforeValidator(fWidth2hWidth)]
